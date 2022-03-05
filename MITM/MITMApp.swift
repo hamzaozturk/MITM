560b7fc3c3c50408d664f4abf8efe119a9340d54
Hamza Öztürk <hamza.ozturk@busoft.com.tr>
2022-03-06 00:23:06 +0300

    Initial Commit

---
+++ MITM/MITMApp.swift
@@ -0,0 +1,17 @@
+//
+//  MITMApp.swift
+//  MITM
+//
+//  Created by Hamza Öztürk on 6.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MITMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
